db5637f66e2c63487e2178d58a7cd0a96d8b4cde
Shantanu yadav <shantanuytech@gmail.com>
2025-07-12 01:34:39 +0530

    implemented the cocktail

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar';
 import gsap from 'gsap';
 import {ScrollTrigger,SplitText} from 'gsap/all'
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const  App = () => {
   return (
     <main>
         <Navbar/>
         <Hero/>
+        <Cocktails/>
        
     </main>
   )
