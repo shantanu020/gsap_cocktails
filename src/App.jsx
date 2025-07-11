39f7144e1720b885c3142097e0fe0d0c92e56dbf
Shantanu yadav <shantanuytech@gmail.com>
2025-07-12 02:18:36 +0530

    About done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from 'gsap';
 import {ScrollTrigger,SplitText} from 'gsap/all'
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const  App = () => {
   return (
@@ -11,6 +12,7 @@ const  App = () => {
         <Navbar/>
         <Hero/>
         <Cocktails/>
+        <About/>
        
     </main>
   )
